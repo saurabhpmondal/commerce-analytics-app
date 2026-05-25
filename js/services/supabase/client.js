32a06d0b4ae9be4f2ab242f8c05991f4ee8987e4
saurabhpmondal <saurabhpmondal@gmail.com>
2026-05-25 14:22:18 +0530

    Update client.js

---
+++ js/services/supabase/client.js
@@ -0,0 +1,11 @@
+import { createClient } from '@supabase/supabase-js';
+
+import {
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+} from '../../config/supabaseConfig.js';
+
+export const supabase = createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
