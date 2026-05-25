0ad231a4752ca508c83684f43d9a93861f20e30d
saurabhpmondal <saurabhpmondal@gmail.com>
2026-05-25 16:41:07 +0530

    Update state.js

--- js/core/state.js
+++ js/core/state.js
@@ -1,1 +1,15 @@
-export const state = {};
+export const state = {
+
+  filters: {
+
+    dateRange: 'This Month',
+
+    brand: 'All Brands',
+
+    erpStatus: 'All Status',
+
+    articleType: 'All Article Types',
+
+    search: ''
+  }
+};
