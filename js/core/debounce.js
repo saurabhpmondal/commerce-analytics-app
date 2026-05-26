7cd5cfcc5d3ee2bdc66e619732543f8d4f3fb361
saurabhpmondal <saurabhpmondal@gmail.com>
2026-05-26 12:57:01 +0530

    Create debounce.js

---
+++ js/core/debounce.js
@@ -0,0 +1,14 @@
+export function debounce(fn, delay) {
+
+  let timeout;
+
+  return (...args) => {
+
+    clearTimeout(timeout);
+
+    timeout = setTimeout(
+      () => fn(...args),
+      delay
+    );
+  };
+}
